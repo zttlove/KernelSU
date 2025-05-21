8fa3547a4293fe150dd8892860e3362fd9ae8de0
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-21 18:59:43 +0800

    kernel: compat: no ext4_unregister_sysfs, no problem
    If ext4_unregister_sysfs ain't there, we don't care.
    
    This is mostly for UL builds.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.c
+++ kernel/kernel_compat.c
@@ -93,3 +93,10 @@ __weak long copy_from_user_nofault(void *dst, const void __user *src, size_t siz
 	return 0;
 }
 #endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 0) || !defined(CONFIG_EXT4_FS)
+__weak void ext4_unregister_sysfs(struct super_block *sb)
+{
+	pr_info("%s: feature not implemented!\n", __func__);
+}
+#endif
