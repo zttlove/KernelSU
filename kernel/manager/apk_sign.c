17face7ec99f202a5ca16c5b09fde2546766a79b
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-14 16:46:49 +0800

    kernel: apk_sign: migrate generic_file_llseek -> vfs_llseek
    seems it has the same abi anyway and this is what syscalls use
    this is to handle shitty backports common on 3.x
    vfs_llseek falls back to generic anyway depending on filesystem's f_op
    
    https://elixir.bootlin.com/linux/v3.10.108/source/fs/read_write.c#L225
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -196,7 +196,7 @@ static __always_inline bool check_v2_signature(char *path,
 	// https://en.wikipedia.org/wiki/Zip_(file_format)#End_of_central_directory_record_(EOCD)
 	for (i = 0;; ++i) {
 		unsigned short n;
-		pos = generic_file_llseek(fp, -i - 2, SEEK_END);
+		pos = vfs_llseek(fp, -i - 2, SEEK_END);
 		kernel_read(fp, &n, 2, &pos);
 		if (n == i) {
 			pos -= 22;
