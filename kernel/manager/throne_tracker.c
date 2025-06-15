1b449a3733e3b9e9c007ce7f0f58e92a1d6f9d4b
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-06-15 20:15:39 +0800

    kernel: throne_tracker: handle filldir_t ABI mismatch on <= 3.18
    Clang splats the following:
    
    drivers/kernelsu/throne_tracker.c:237:47: error: incompatible function pointer types initializing 'const filldir_t' (aka 'int (*const)(void *, const char *, int, long long, unsigned long long, unsigned int)') with an expression of type 'int (struct dir_context *, const char *, int, loff_t, u64, unsigned int)' (aka 'int (struct dir_context *, const char *, int, long long, unsigned long long, unsigned int)') [-Wincompatible-function-pointer-types]
      237 |                         struct my_dir_context ctx = { .ctx.actor = my_actor,
          |                                                                    ^~~~~~~~
    1 error generated.
    
    reference:
    - 3.18: https://elixir.bootlin.com/linux/v3.18/source/include/linux/fs.h#L1469
    - 3.19: https://elixir.bootlin.com/linux/v3.19/source/include/linux/fs.h#L1489
    
    so just pass as void, then cast it back
    
    https://github.com/ximi-libra-test/android_kernel_xiaomi_libra/commit/036c5326dd63046ef64288b027d47f084b50c3ee
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/manager/throne_tracker.c
+++ kernel/manager/throne_tracker.c
@@ -64,11 +64,21 @@ struct my_dir_context {
 #define FILLDIR_ACTOR_STOP -EINVAL
 #endif
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,19,0)
+#define MY_ACTOR_CTX_ARG struct dir_context *ctx
+#else
+#define MY_ACTOR_CTX_ARG void *ctx_void
+#endif
+
 extern bool is_manager_apk(char *path);
-FILLDIR_RETURN_TYPE my_actor(struct dir_context *ctx, const char *name,
+FILLDIR_RETURN_TYPE my_actor(MY_ACTOR_CTX_ARG, const char *name,
 			     int namelen, loff_t off, u64 ino,
 			     unsigned int d_type)
 {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3,19,0)
+	// then pull it out of the void
+	struct dir_context *ctx = (struct dir_context *)ctx_void;
+#endif
 	struct my_dir_context *my_ctx =
 		container_of(ctx, struct my_dir_context, ctx);
 
