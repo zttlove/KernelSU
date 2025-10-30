f80ed4e4836f89bb24c5d777ebc8133d226e1940
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-31 05:20:15 +0800

    kernel: supercall: provide sys_reboot handler
    well, upstream isn't providing one usable for manual hook,
    so lets make our own.
    
    and yes passing arg as ** / reference of arg pointer can help us to
    pull off threestarprogrammer moments.
    
    Usage:
    	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
    
    on sys_reboot's entry.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/supercall/supercall.c
+++ kernel/supercall/supercall.c
@@ -46,6 +46,42 @@ int ksu_install_fd(void)
 	return fd;
 }
 
+// downstream: make sure to pass arg as reference, this can allow us to extend things.
+int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg)
+{
+	if (magic1 != KSU_INSTALL_MAGIC1)
+		return 0;
+
+	// when ternary on fmt?
+	// cold syscall, we can splurge xD
+	if (magic2 == KSU_INSTALL_MAGIC2)
+		pr_info("sys_reboot: magic: 0x%x id: 0x%x pid: %d comm: %s \n", magic1, magic2, current->pid, current->comm);
+	else
+		pr_info("sys_reboot: magic: 0x%x id: %d pid: %d pid: %s \n", magic1, magic2, current->pid, current->comm);
+
+	// arg4 = (unsigned long)PT_REGS_SYSCALL_PARM4(real_regs);
+	// downstream: dereference arg as arg4 so we can be inline to upstream
+	void __user *arg4 = (void __user *)*arg;
+
+	// Check if this is a request to install KSU fd
+	if (magic2 == KSU_INSTALL_MAGIC2) {
+		int fd = ksu_install_fd();
+		pr_info("[%d] install ksu fd: %d\n", current->pid, fd);
+
+		if (copy_to_user((void __user *)arg4, &fd, sizeof(fd))) {
+			pr_err("install ksu fd reply err\n");
+		}
+
+		return 0;
+	}
+
+	// grab a copy as we write the pointer on the pointer
+	// u64 reply = (u64)*arg;	
+	// extensions
+
+	return 0;
+}
+
 void __init ksu_supercalls_init(void)
 {
 	ksu_supercall_dump_commands();
