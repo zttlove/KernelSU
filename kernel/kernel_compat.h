2d505af284eb71dc47a01bedb2d212186b48afed
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-08 19:54:37 +0800

    kernel: compat: file_inode compat for < 3.9
    https://github.com/torvalds/linux/commit/496ad9aa8ef448058e36ca7a787c61f2e63f0f54
    
    file_inode() on file_wrapper
    
    old: https://github.com/backslashxx/KernelSU/commit/c817e06c243e7eef77a312ef895fa463e3c28d0e
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -263,4 +263,8 @@ __weak char *bin2hex(char *dst, const void *src, size_t count)
 }
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 9, 0)
+#define file_inode(f) ((f)->f_path.dentry->d_inode)
+#endif
+
 #endif // __KSU_H_KERNEL_COMPAT
