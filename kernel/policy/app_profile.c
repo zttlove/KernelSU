5b0803a3911b75875a4d3d81fa2ceba03f2d7c6d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-13 08:22:18 +0800

    HACK: kernel: bypass tf out of uid_t/gid_t strict type checks
    now CONFIG_UIDGID_STRICT_TYPE_CHECKS kconf wont matter on ultra legacy.
    
    since these are like
    
    typedef struct {
    	uid_t val;
    } kuid_t;
    
    typedef struct {
    	gid_t val;
    } kgid_t;
    
    and then
    
    typedef __kernel_uid32_t	uid_t;
    typedef __kernel_gid32_t	gid_t;
    
    which is
    
    typedef unsigned int	__kernel_uid32_t;
    typedef unsigned int	__kernel_gid32_t;
    
    on ultra legacy and/or when CONFIG_UIDGID_STRICT_TYPE_CHECKS is disabled
    its like
    
    typedef uid_t kuid_t;
    typedef gid_t kgid_t;
    
    so we can just force ourselves in by type-punning the fuck out of it.
    
    old version: https://github.com/tiann/KernelSU/commit/87d8219a1613244709c30488babf8dc519639327
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/policy/app_profile.c
+++ kernel/policy/app_profile.c
@@ -119,22 +119,22 @@ static int escape_to_root(bool is_forced)
 		return -ENOMEM;
 	}
 
-	if (!is_forced && cred->euid.val == 0) {
+	if (!is_forced && ksu_get_uid_t(cred->euid) == 0) {
 		pr_warn("Already root, don't escape!\n");
 		goto out_abort_creds;
 	}
 
-	profile = ksu_get_root_profile(cred->uid.val);
+	profile = ksu_get_root_profile(ksu_get_uid_t(cred->uid));
 
-	cred->uid.val = profile->uid;
-	cred->suid.val = profile->uid;
-	cred->euid.val = profile->uid;
-	cred->fsuid.val = profile->uid;
+	ksu_get_uid_t(cred->uid) = profile->uid;
+	ksu_get_uid_t(cred->suid) = profile->uid;
+	ksu_get_uid_t(cred->euid) = profile->uid;
+	ksu_get_uid_t(cred->fsuid) = profile->uid;
 
-	cred->gid.val = profile->gid;
-	cred->fsgid.val = profile->gid;
-	cred->sgid.val = profile->gid;
-	cred->egid.val = profile->gid;
+	ksu_get_uid_t(cred->gid) = profile->gid;
+	ksu_get_uid_t(cred->fsgid) = profile->gid;
+	ksu_get_uid_t(cred->sgid) = profile->gid;
+	ksu_get_uid_t(cred->egid) = profile->gid;
 	cred->securebits = 0;
 
 	BUILD_BUG_ON(sizeof(profile->capabilities.effective) != sizeof(kernel_cap_t));
