1567aab68acfab7063bad85ed57ff909303d5341
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-14 18:59:31 +0800

    kernel: lsm_hook: introduce selinux_ops LSM tampering for < 4.2
    since 3.x kernels doesnt have the same capability as > 4.1 to stack LSM's
    we introduce a way to "stack" hooks on top of selinux LSM.
    
    security_operations is just a struct of function pointers.
    
    ```
    struct security_operations {
    	...
    
    	int (*ptrace_access_check) (struct task_struct *child, unsigned int mode);
    	int (*ptrace_traceme) (struct task_struct *parent);
    	...
    ```
    
    and selinux owns one which has
    
    ```
    static struct security_operations selinux_ops = {
    	...
    
    	.ptrace_access_check =		selinux_ptrace_access_check,
    	.ptrace_traceme =		selinux_ptrace_traceme,
    	...
    ```
    
    so as long as we can get the address of selinux_ops we can
    1. redirect actual operation to a fn_pointer we control
    2. create a hook_fn which hooks on entry and returns original operation
    3. redirect the operation to a hook_fn we control
    
    but this "stacking" on legacy kernels (pre-4.2) was a bit of a meme since we
    have manually export selinux_ops on security/selinux/hooks.c.
    
    Now, we make this edit optional. We hunt for selinux_ops in memory instead.
    
    Methodology:
    1. use "anchors" (like selinux_enabled or avc_cache_threshold) as starting
       points and scan 10k ptr up/down.
    2. check for the "selinux" string at the start of the struct.
    3. check "pointer density" - basically making sure the struct is full of
       functuion pointers pointing to kernel executable text area.
    4. assign a function pointer to selinux_cred_free and call it with a
       dummy cred, if it behaves properly, thats it.
    
    This also adds a backport of copy_from_kernel_nofault, which we dont really need,
    we can use probe_kernel_read, but a lot has weird backprots, it is small enough
    anyway so we just bring it in.
    
    EDIT: reg and unreg inside stop_machine
    
    old: https://github.com/tiann/KernelSU/commit/0eec4c1ffb2c9a5b2b188a51a602c4a71f9810a5
    https://github.com/tiann/KernelSU/commit/1e31d20acc1b3c21febca5a1a138b93461700898
    
    devlog:
    https://github.com/backslashxx/KernelSU/commit/85c624ce54b1369b764a0f14162406988c762b17
    https://github.com/backslashxx/KernelSU/compare/217d1d4e5bde1ce5c55784d00caa723b06c0ec8e...2a9fa4c81c224ef468eef03982247872cb6cfff5
    https://github.com/backslashxx/KernelSU/activity?ref=staging-selinux_ops-scan
    
    EDIT: wire-up setprocattr for 3.x
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.c
+++ kernel/kernel_compat.c
@@ -1,1 +1,41 @@
-// placeholder
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0)
+__weak long copy_from_kernel_nofault(void *dst, const void *src, size_t size)
+{
+	// https://elixir.bootlin.com/linux/v5.2.21/source/mm/maccess.c#L27
+	long ret;
+	mm_segment_t old_fs = get_fs();
+
+	set_fs(KERNEL_DS);
+	pagefault_disable();
+	ret = __copy_from_user_inatomic(dst,
+			(__force const void __user *)src, size);
+	pagefault_enable();
+	set_fs(old_fs);
+
+	return ret ? -EFAULT : 0;
+}
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0) 
+__weak long copy_from_user_nofault(void *dst, const void __user *src, size_t size)
+{
+	// https://elixir.bootlin.com/linux/v5.8/source/mm/maccess.c#L205
+	long ret = -EFAULT;
+	mm_segment_t old_fs = get_fs();
+
+	set_fs(USER_DS);
+
+	// normally theres an access_ok check here
+	// but for what we use it, it will always be true.
+	// so we skip it
+	pagefault_disable();
+	ret = __copy_from_user_inatomic(dst, src, size);
+	pagefault_enable();
+
+	set_fs(old_fs);
+
+	if (ret)
+		return -EFAULT;
+	return 0;
+}
+#endif
