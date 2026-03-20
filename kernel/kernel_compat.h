a67051e41db8f50d8ac514cf55e627aee3d13209
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-20 20:46:01 +0800

    kernel: adapt "namespace support" feature to old kernels
    TODO: add comments
    
    devlog and shit
    https://github.com/backslashxx/KernelSU/commit/6d100ac252baeb3c3bef8e94255e0d846e8d6921
    https://github.com/backslashxx/KernelSU/tree/384de6420b64850a4cf2abbb83171f151a851c7f
    https://github.com/ximi-mojito-test/mojito_krenol/commit/33ae97fb83f976ad82e73e7bbe65a421946f51d8
    https://github.com/ximi-libra-test/android_kernel_xiaomi_libra/tree/52810d22428875cca163cb52fd2819120e0b8120
    
    https://github.com/backslashxx/KernelSU/commit/7866f4e83468de715fa1a97b4e405b389b9b3c8c *
    
    upstream: https://github.com/tiann/KernelSU/pull/2909
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -69,4 +69,12 @@ static inline void ksu_kvfree(void *buf)
 #define close_fd sys_close
 #endif
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 6, 0)
+static inline struct file *ksu_dentry_open(const struct path *path, int flags, const struct cred *cred)
+{
+	return dentry_open((*path).dentry, (*path).mnt, flags, cred);
+}
+#define dentry_open ksu_dentry_open
+#endif
+
 #endif // __KSU_H_KERNEL_COMPAT
