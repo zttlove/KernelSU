af0c83385709c3ea4abb04f2c1843ae3de7afce9
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-20 20:46:01 +0800

    kernel: adapt "namespace support" feature to old kernels
    TODO: add comments
    
    devlog and shit
    https://github.com/backslashxx/KernelSU/commit/6d100ac252baeb3c3bef8e94255e0d846e8d6921
    https://github.com/backslashxx/KernelSU/tree/384de6420b64850a4cf2abbb83171f151a851c7f
    https://github.com/ximi-mojito-test/mojito_krenol/commit/33ae97fb83f976ad82e73e7bbe65a421946f51d8
    https://github.com/ximi-libra-test/android_kernel_xiaomi_libra/tree/52810d22428875cca163cb52fd2819120e0b8120
    
    https://github.com/backslashxx/KernelSU/commit/7866f4e83468de715fa1a97b4e405b389b9b3c8c *
    
    upstream: https://github.com/tiann/KernelSU/pull/2909
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/infra/su_mount_ns.c
+++ kernel/infra/su_mount_ns.c
@@ -2,10 +2,13 @@ extern int path_mount(const char *dev_name, struct path *path,
 					  const char *type_page, unsigned long flags,
 					  void *data_page);
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
 #if defined(__aarch64__)
 extern long __arm64_sys_setns(const struct pt_regs *regs);
 #elif defined(__x86_64__)
 extern long __x64_sys_setns(const struct pt_regs *regs);
+#elif defined(__arm__) // https://syscalls.mebeim.net/?table=arm/32/eabi/latest
+extern long sys_setns(const struct pt_regs *regs);
 #endif
 
 static long ksu_sys_setns(int fd, int flags)
@@ -20,10 +23,16 @@ static long ksu_sys_setns(int fd, int flags)
 	return __arm64_sys_setns(&regs);
 #elif defined(__x86_64__)
 	return __x64_sys_setns(&regs);
+#elif defined(__arm__)
+	return sys_setns(&regs);
 #else
-#error "Unsupported arch"
+	return -ENOSYS;
 #endif
 }
+#else
+#define ksu_sys_setns sys_setns
+#define ksys_unshare sys_unshare
+#endif // > 4.17
 
 // global mode , need CAP_SYS_ADMIN and CAP_SYS_CHROOT to perform setns
 static void ksu_mnt_ns_global(void)
@@ -51,6 +60,7 @@ static void ksu_mnt_ns_global(void)
 		pwd_path = NULL;
 	}
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 19, 0)
 try_setns:
 
 	rcu_read_lock();
@@ -70,12 +80,31 @@ try_setns:
 		goto out;
 	}
 	struct path ns_path;
-	long ret = ns_get_path(&ns_path, pid1_task, &mntns_operations);
+	long ret = (long)ns_get_path(&ns_path, pid1_task, &mntns_operations);
 	put_task_struct(pid1_task);
 	if (ret) {
 		pr_warn("failed get path for init mount namespace: %ld\n", ret);
 		goto out;
 	}
+#else
+try_setns:
+	;
+	// on UL kernels we can try to just feed it with struct path of /proc/1/ns/mnt
+	// we do NOT have ns_get_path. if it works, GOOD. if it doesn't I don't care.
+	struct path ns_path;
+	const struct cred *saved = override_creds(ksu_cred);
+
+	// make sure to LOOKUP_FOLLOW
+	// /proc/1/ns/mnt -> 'mnt:[4026531840]'
+	long ret = kern_path("/proc/1/ns/mnt", LOOKUP_FOLLOW, &ns_path);
+	if (ret) {
+		revert_creds(saved);
+		pr_warn("kern_path /proc/1/ns/mnt fail! ret: %d\n", ret);
+		goto out;
+	}
+	revert_creds(saved);
+#endif
+
 	struct file *ns_file = dentry_open(&ns_path, O_RDONLY, ksu_cred);
 
 	path_put(&ns_path);
@@ -95,11 +124,7 @@ try_setns:
 	fd_install(fd, ns_file);
 	ret = ksu_sys_setns(fd, CLONE_NEWNS);
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 11, 0)
-	ksys_close(fd);
-#else
 	close_fd(fd);
-#endif
 
 	if (ret) {
 		pr_warn("call setns failed: %ld\n", ret);
