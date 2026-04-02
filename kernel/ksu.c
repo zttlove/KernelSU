1ea0d5f6607eb43f1170967da77068b7eac4cec0
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-02 09:42:45 +0800

    kernel: feature: sulog: adjust for downstream
    on upstream they break this up to before and after escape_to_root
    here we just assume that its always successful, so we can skip
    request + confirmation.
    on upstream they only put this on execve's path,  so we only call
    this when caller wants to escalate.
    
    we can get argv of calling process on bprm by pulling it on current->mm
    argv is already kind of pre-flattened for us, we only need to replace null terminators
    with spaces after the first one.
    
    on upstream they pass filename + argv[x], here we pass argv0 and argv[x]
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -138,6 +138,10 @@ int __init kernelsu_init(void)
 
 	ksu_kernel_umount_init(); // so the feature is registered
 
+#ifdef CONFIG_KSU_FEATURE_SULOG	
+	ksu_sulog_init(); // so the feature is registered
+#endif
+
 	ksu_core_init();
 
 #if defined(CONFIG_KSU_KPROBES_KSUD) && !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE)
