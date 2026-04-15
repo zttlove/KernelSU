a3db289b55baa317414f746288d429f55eb43e45
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-16 01:09:29 +0800

    kernel: compat: handle UL compat for refactored allowlist
    for: https://github.com/tiann/KernelSU/pull/3418
    
    ref: https://github.com/torvalds/linux/commit/b67bfe0d42cac56c512dd5da4b1b347a23f4b70a
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -345,4 +345,15 @@ static noinline ssize_t ksu_kernel_write_compat(struct file *p, const void *buf,
 
 static inline void ksu_kfree_byref(void *buf) { kfree(*(void **)buf); }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION (3, 9, 0)
+// hashtable.h, list.h, rculist.h
+// ref: https://github.com/torvalds/linux/commit/b67bfe0d42cac56c512dd5da4b1b347a23f4b70a
+#include "linux_hashtable.h"
+static inline int __must_check ksu_kref_get_unless_zero(struct kref *kref)
+{ 
+	return atomic_add_unless(&kref->refcount, 1, 0); 
+}
+#define kref_get_unless_zero ksu_kref_get_unless_zero
+#endif // < 3.9
+
 #endif // __KSU_H_KERNEL_COMPAT
