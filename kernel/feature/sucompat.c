b485fd08dfc2aa0228890181ff0e336fa45faf95
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-04-25 20:46:57 +0800

    kernel: build: sucompat: hide / inline hook fns on syscall table hook
    related: https://github.com/backslashxx/KernelSU/commit/7d33689f67ef447d78e54ec9b4a1ab8a6c943530
    
    cant just flatten as some kernels already have heavy inlining by def.
    lets respect kernel kbuild instead
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/feature/sucompat.c
+++ kernel/feature/sucompat.c
@@ -1,3 +1,9 @@
+#ifdef CONFIG_KSU_TAMPER_SYSCALL_TABLE
+#define SUCOMPAT_HOOK_TYPE static __always_inline int
+#else
+#define SUCOMPAT_HOOK_TYPE int
+#endif
+
 #define SU_PATH "/system/bin/su"
 #define SH_PATH "/system/bin/sh"
 
@@ -216,7 +222,7 @@ no_escalate:
 }
 
 // sys_faccessat
-int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *__unused_flags)
+SUCOMPAT_HOOK_TYPE ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode, int *__unused_flags)
 {
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
@@ -226,7 +232,7 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 }
 
 // sys_newfstatat, sys_fstat64
-int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
+SUCOMPAT_HOOK_TYPE ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 {
 	if (!is_su_allowed((const void **)filename_user))
 		return 0;
@@ -236,7 +242,7 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 }
 
 // sys_execve, compat_sys_execve
-int ksu_handle_execve(const char __user **filename_user, void *argv, void *envp)
+SUCOMPAT_HOOK_TYPE ksu_handle_execve(const char __user **filename_user, void *argv, void *envp)
 {
 
 #ifdef CONFIG_KSU_FEATURE_ADBROOT
@@ -250,6 +256,7 @@ int ksu_handle_execve(const char __user **filename_user, void *argv, void *envp)
 	return 0;
 }
 
+#ifndef CONFIG_KSU_TAMPER_SYSCALL_TABLE
 static __always_inline void ksu_sucompat_kernel_common(void **filename_ptr, void *argv, void *envp, const char *function_name)
 {
 
@@ -332,6 +339,7 @@ int ksu_legacy_execve_sucompat(const char **filename_ptr, void *argv, void *envp
 	return 0;
 }
 #endif
+#endif // CONFIG_KSU_TAMPER_SYSCALL_TABLE
 
 #ifdef CONFIG_KSU_TAMPER_SYSCALL_TABLE
 static void syscall_table_sucompat_enable();
