67ad5c56d6faf58633ba153a5291740061f01e29
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-05-17 14:17:27 +0800

    kernel: lsm_hook: wire up setprocattr lsm hack for selinux_hide
    devlog:
    https://github.com/backslashxx/KernelSU/commit/e181dd7ec583d162666636d9dc9e836ce1732cb2
    https://github.com/backslashxx/KernelSU/commit/444264ff7cc651c5ad398a98d02da6da12dae985
    https://github.com/backslashxx/KernelSU/commit/868ce4b55a50ed62ae0e47a03a4e59a5558958b9
    
    TODO: proper commetn and notes
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/hook/lsm_hooks_static.c
+++ kernel/hook/lsm_hooks_static.c
@@ -94,6 +94,14 @@ static __nocfi ssize_t ksu_vfs_read(struct file *file, char __user *buf, size_t 
 	return vfs_read(file, buf, count, pos);
 }
 
+// setprocattr
+extern int security_setprocattr(int lsmid, const char *name, void *value, size_t size);
+__attribute__((hot))
+static __nocfi int ksu_setprocattr(int lsmid, const char *name, void *value, size_t size)
+{
+	ksu_hide_setprocattr(name, value, size);
+	return security_setprocattr(lsmid, name, value, size);
+}
 
 static int lsm_hunt_symbol_callsite(uintptr_t target_callsite, ptrdiff_t target_width, uintptr_t symbol_addr, uintptr_t hook_addr)
 {
@@ -222,6 +230,14 @@ static void __init ksu_core_init(void)
 
 	ret = lsm_hunt_symbol_callsite(target_callsite, 64 * sizeof(void *), symbol_addr, (uintptr_t)&ksu_vfs_read);
 	pr_info("lsm_hijack: ksys_read: ret %d \n", ret);
+	symbol_addr = NULL;
+
+	// TODO: traverse proc_pid_attr_operations
+	target_callsite = (uintptr_t)kallsyms_lookup_name("proc_pid_attr_write");
+	symbol_addr = (uintptr_t)&security_setprocattr;
+
+	ret = lsm_hunt_symbol_callsite(target_callsite, 64 * sizeof(void *), symbol_addr, (uintptr_t)&ksu_setprocattr);
+	pr_info("lsm_hijack: security_setprocattr: ret %d \n", ret);
 	symbol_addr = NULL;
 
 }
