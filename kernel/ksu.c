e3b1f537540b566b4d033bc4e3f560e569c0dc17
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-05-24 23:34:10 +0800

    kernel: branch_link_hook: introduce arm64 branch link callsite hijacking
    Syscall table tampering is either fucking slow or completely broken on
    modern kernels with CFI.
    
    This introduces an experimental, optprobes-style hooking that scans
    kernel-executable-text and hot-patches ARM64 'bl' insn directly.
    This allows us to hijack execution straight at callsite, bypassing CFI
    fuckups with near-zero overhead.
    
    Verified on 6.12 GKI.
    
    NOTES:
    - ARM64 only.
    - not wired for pre-4.17 unless you write compat yourself. wontfix.
      - use KSU_TAMPER_SYSCALL_TABLE instead.
    - might be spotty on de-GKI/partially-de-GKI modern kernels (unstable symbols)
    - 4.19/5.4 compat is probably broken but it's in there, tofix, maybe.
    
    devlog:
    https://github.com/tiann/KernelSU/compare/da70d416108259061db171946bb6363db59e8799...5194ab15f15abca5ba1329cf1aeb36955359e29f
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/ksu.c
+++ kernel/ksu.c
@@ -102,6 +102,10 @@
 #endif
 #endif /* CONFIG_KSU_TAMPER_SYSCALL_TABLE */
 
+#ifdef CONFIG_KSU_HACK_ARM64_BRANCH_LINK
+#include "hook/branch_link_hook_arm64.c"
+#endif
+
 #if defined(CONFIG_KSU_KPROBES_KSUD) && !defined(CONFIG_KSU_TAMPER_SYSCALL_TABLE)
 #include "hook/kp_ksud.c"
 #endif
@@ -154,6 +158,10 @@ static int __init kernelsu_init(void)
 
 #ifdef CONFIG_KSU_TAMPER_SYSCALL_TABLE
 	ksu_syscall_table_hook_init();
+#endif
+
+#ifdef CONFIG_KSU_HACK_ARM64_BRANCH_LINK
+	ksu_branch_link_patch_init();
 #endif
 
 	return 0;
